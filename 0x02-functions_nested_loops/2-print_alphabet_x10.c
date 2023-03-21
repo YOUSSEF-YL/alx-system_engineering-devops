8772cd595c5e5098f8bcaa5b7f6f9f9b08414dbe
YOUSSEF-YL <youssef.bachar7@gmail.com>
2023-03-21 19:05:47 +0000

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print _putchar
+ *
+ * Return: 0 (SUCCESS)
+ */
+void print_alphabet_x10(void)
+{
+	int	a;
+	int	i;
+
+	i = 0;
+	while (i != 10)
+	{
+		a = 97;
+		while (a <= 'z')
+			putchar(a++);
+		putchar('\n');
+		i++;
+	}
+}
